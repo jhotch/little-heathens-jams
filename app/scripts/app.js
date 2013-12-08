26f966230a486080320ea75d24c79913a4efad30
James Hotchkin <james.hotchkin@gmail.com>
2013-12-08 03:27:05 -0600

    applying one-page-wonder template and images

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,4 +16,22 @@ angular.module('littleHeathensJamsApp', [
         redirectTo: '/'
       });
     $locationProvider.html5Mode(true);
-  });
+  })
+  .directive('scrollOnClick', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, $elm, attrs) {
+      var idToScroll = attrs.href;
+      $elm.on('click', function(e) {
+        e.preventDefault();
+        var $target;
+        if (idToScroll) {
+          $target = $(idToScroll);
+        } else {
+          $target = $elm;
+        }
+        $('body').animate({scrollTop: $target.offset().top}, 'slow');
+      });
+    }
+  };
+});
